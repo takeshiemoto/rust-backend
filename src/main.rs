73147e4d49ab69fa8abe22d6dfd7e6347699e17a
takeshiemoto <private.takeshiemoto@gmail.com>
2023-04-07 10:12:44 +0900

    feat: add endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use warp::Filter;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8081))?
-    .run()
-    .await
+#[tokio::main]
+async fn main() {
+    let get_players = warp::get()
+        .and(warp::path("players"))
+        .and(warp::path::end())
+        .and_then(get_players);
+
+    let get_player = warp::get()
+        .and(warp::path("players"))
+        .and(warp::path::param::<u32>())
+        .and(warp::path::end())
+        .and_then(get_player);
+
+    let post_player = warp::post()
+        .and(warp::path("players"))
+        .and(warp::path::end())
+        .and_then(post_player);
+
+    let put_player = warp::put()
+        .and(warp::path("players"))
+        .and(warp::path::param::<u32>())
+        .and(warp::path::end())
+        .and_then(put_player);
+
+    let delete_player = warp::delete()
+        .and(warp::path("players"))
+        .and(warp::path::param::<u32>())
+        .and(warp::path::end())
+        .and_then(delete_player);
+
+    let routes = get_players
+        .or(get_player)
+        .or(post_player)
+        .or(put_player)
+        .or(delete_player);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+async fn get_players() -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Get players"))
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn get_player(id: u32) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Get players by id {}", id))
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn post_player() -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Post players"))
+}
+
+async fn put_player(id: u32) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Put players by id {}", id))
+}
+
+async fn delete_player(id: u32) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Delete players by id {}", id))
 }
