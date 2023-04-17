513835c72967c9824539f8bb5ec43fcb5c3e996e
takeshiemoto <private.takeshiemoto@gmail.com>
2023-04-17 11:54:01 +0900

    user handler

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            web::scope("/api").service(
+                web::scope("/v1").service(
+                    web::scope("/users")
+                        .route("", web::get().to(get_users))
+                        .route("", web::post().to(create_users))
+                        .route("/{id}", web::get().to(get_users_by_id))
+                        .route("/{id}", web::put().to(update_users))
+                        .route("/{id}", web::delete().to(delete_users)),
+                ),
+            ),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn get_users() -> impl Responder {
+    HttpResponse::Ok().body("get_users")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn get_users_by_id() -> impl Responder {
+    HttpResponse::Ok().body("get_users_by_id")
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn create_users() -> impl Responder {
+    HttpResponse::Ok().body("create_users")
+}
+
+async fn update_users() -> impl Responder {
+    HttpResponse::Ok().body("update_users")
+}
+
+async fn delete_users() -> impl Responder {
+    HttpResponse::Ok().body("delete_user")
 }
