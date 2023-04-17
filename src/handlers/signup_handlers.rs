d1be42de7e614bcb47bf01d71acd51ed04821b9a
takeshiemoto <private.takeshiemoto@gmail.com>
2023-04-18 06:42:50 +0900

    Add validation for request body

--- src/handlers/signup_handlers.rs
+++ src/handlers/signup_handlers.rs
@@ -1,16 +1,27 @@
 use crate::models::app_state::AppState;
-use crate::models::signup_request::SignupRequest;
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, Error, HttpResponse, Responder};
 use bcrypt::{hash, DEFAULT_COST};
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct SignupRequest {
+    #[validate(email(message = "Invalid email address"))]
+    pub email: String,
+    #[validate(length(min = 4, message = "Password must be at least 4 characters long"))]
+    pub password: String,
+}
 
 pub async fn signup(
     req: web::Json<SignupRequest>,
     app_state: web::Data<AppState>,
-) -> impl Responder {
+) -> Result<HttpResponse, Error> {
+    if let Err(e) = req.validate() {
+        return Ok(HttpResponse::BadRequest().json(e));
+    }
+
     let email = req.email.clone();
     let password = hash(req.password.clone(), DEFAULT_COST).unwrap();
-
-    println!("email: {}, password: {}", email, password);
 
     sqlx::query("INSERT INTO users (email, password) VALUES ($1, $2)")
         .bind(email.clone())
@@ -19,7 +30,7 @@ pub async fn signup(
         .await
         .unwrap();
 
-    HttpResponse::Ok().body("signup_complete")
+    Ok(HttpResponse::Ok().finish())
 }
 
 pub async fn signup_complete() -> impl Responder {
