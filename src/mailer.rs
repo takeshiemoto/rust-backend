e630d7374c330e7470f7718520b4d27d4e5c71cf
takeshiemoto <private.takeshiemoto@gmail.com>
2023-05-01 20:37:10 +0900

    refactor: mailer error handling

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,18 +1,25 @@
+use derive_more::{Display, Error};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 use std::env;
 
-pub async fn send_mail(message: &Message) -> Result<(), String> {
-    let api_key = env::var("SENDGRID_API_KEY").map_err(|e| e.to_string())?;
+#[derive(Debug, Display, Error)]
+pub enum SendMailError {
+    EnvVar(env::VarError),
+    Lelay(lettre::transport::smtp::Error),
+    Send(lettre::transport::smtp::Error),
+}
+
+pub async fn send_mail(message: &Message) -> Result<(), SendMailError> {
+    let api_key = env::var("SENDGRID_API_KEY").map_err(SendMailError::EnvVar)?;
     let creds = Credentials::new("apikey".to_string(), api_key);
 
     let mailer = SmtpTransport::relay("smtp.sendgrid.net")
-        .map_err(|e| e.to_string())?
+        .map_err(SendMailError::Lelay)?
         .credentials(creds)
         .build();
 
-    if let Err(e) = mailer.send(message) {
-        return Err(e.to_string());
-    }
+    mailer.send(message).map_err(SendMailError::Send)?;
+
     Ok(())
 }
