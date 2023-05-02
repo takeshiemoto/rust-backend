363f936d85eef6948045e845276718439af335b2
takeshiemoto <private.takeshiemoto@gmail.com>
2023-05-02 13:49:04 +0900

    refactor: ussing from trait

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,21 +1,33 @@
 use derive_more::{Display, Error};
 use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::Error;
 use lettre::{Message, SmtpTransport, Transport};
 use std::env;
+use std::env::VarError;
 
 #[derive(Debug, Display, Error)]
 pub enum SendMailError {
-    EnvVar(env::VarError),
-    Lelay(lettre::transport::smtp::Error),
+    EnvVar(VarError),
     Send(lettre::transport::smtp::Error),
 }
 
+impl From<VarError> for SendMailError {
+    fn from(value: VarError) -> Self {
+        Self::EnvVar(value)
+    }
+}
+
+impl From<lettre::transport::smtp::Error> for SendMailError {
+    fn from(value: Error) -> Self {
+        Self::Send(value)
+    }
+}
+
 pub async fn send_mail(message: &Message) -> Result<(), SendMailError> {
-    let api_key = env::var("SENDGRID_API_KEY").map_err(SendMailError::EnvVar)?;
+    let api_key = env::var("SENDGRID_API_KEY")?;
     let creds = Credentials::new("apikey".to_string(), api_key);
 
-    let mailer = SmtpTransport::relay("smtp.sendgrid.net")
-        .map_err(SendMailError::Lelay)?
+    let mailer = SmtpTransport::relay("smtp.sendgrid.net")?
         .credentials(creds)
         .build();
 
